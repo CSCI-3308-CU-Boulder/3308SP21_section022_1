6fb895f0cba4a1c7677351b1c343f05e6cc47390
spencernikolaeff <59879326+spencernikolaeff@users.noreply.github.com>
2021-04-13 18:24:29 -0600

    add login post, edit register post
    removed variables from register, need to change database specifications, need to have option to register on login page

--- Source Code/index.js
+++ Source Code/index.js
@@ -29,6 +29,24 @@ app.get("/", (req, res) => { //spencer
   });
 });
 
+app.post("/login", (req, res) => { //spencer
+  const un = req.body.username;
+  const pas = req.body.password;
+
+  const searchun = "select UserName from `User` where password = '"+pas+"';";
+
+  db.query(searchun, function (err, result) {
+    if(err) {
+      console.log(err);
+    } else {
+      if(result == un) {
+        res.render("/pages/home");
+      } else {
+        res.send("Incorrect Username or Password!")
+      }
+    }
+  });
+});
 
 app.get("/collection", (req, res) => {
   db.query('select * from games', function(err, rows, fields){
@@ -82,7 +100,7 @@ app.get("/friends", (req, res) => {
 });
 
 app.get("/register", (req, res) => { //Spencer
-  res.render('register.ejs');
+  res.render('/pages/login');
 });
 
 app.post('/register', (req, res) => { //Spencer
@@ -92,53 +110,32 @@ app.post('/register', (req, res) => { //Spencer
   var ln = req.body.lastname; //not null
   var em = req.body.email; //not null
   var dob = req.body.dob; //not null
-  var pn = req.body.phonenumber; //can be empty
-  var city = req.body.city; //can be empty
-  var state = req.body.state; //can be empty
-  var zip = req.body.zip; //can be empty
-  var country = req.body.country; //can be empty
-  var acd = req.body.accountcreationdate; //rest nn
+
+  var nul = "null";
+  
+  //var pn = req.body.phonenumber; //can be empty
+  //var city = req.body.city; //can be empty
+  //var state = req.body.state; //can be empty
+  //var zip = req.body.zip; //can be empty
+  //var country = req.body.country; //can be empty
+  
+  //var acd = req.body.accountcreationdate; //rest nn
+  
   var sq1 = req.body.securityq1;
   var sa1 = req.body.securitya1;
-  var sq2 = req.body.securityq2;
-  var sa2 = req.body.securitya2;
-  var sq3 = req.body.securityq3;
-  var sa3 = req.body.securitya3;
-  var sql = "insert into `User` (UserName, password, FirstName, LastName, Email, DOB, PhoneNumber, City, State, Zip, Country, AccountCreationDate, SecurityQuestion1, Answer1, SecurityQuestion2, Answer2, SecurityQuestion3, Answer3) VALUES ('"+un+"','"+pw+"','"+fn+"','"+ln+"','"+em+"','"+dob+"','"+pn+"','"+city+"', '"+state+"', '"+zip+"','"+country+"','"+acd+"','"+sq1+"','"+sa1+"','"+sq2+"','"+sa2+"','"+sq3+"','"+sa3+"');";
+  
+  //var sq2 = req.body.securityq2;
+  //var sa2 = req.body.securitya2;
+  //var sq3 = req.body.securityq3;
+  //var sa3 = req.body.securitya3;
+  
+  var sql = "insert into `User` (UserName, password, FirstName, LastName, Email, DOB, PhoneNumber, City, State, Zip, Country, AccountCreationDate, SecurityQuestion1, Answer1, SecurityQuestion2, Answer2, SecurityQuestion3, Answer3) VALUES ('"+un+"','"+pw+"','"+fn+"','"+ln+"','"+em+"','"+dob+"','"+nul+"','"+nul+"', '"+nul+"', '"+nul+"','"+nul+"','"+nul+"','"+sq1+"','"+sa1+"','"+nul+"','"+nul+"','"+nul+"','"+nul+"');";
   db.query(sql, function (err, result) {
     if (err) throw err;
     console.log("1 record inserted");
   });
-  res.redirect("/home");
+  res.redirect("/login");
 });
-
-app.get("/account", (req, res) => { //spencer
-  res.render('account.ejs');
-});
-
-app.post('/account:username', (req, res) => { //spencer
-  var uncur = req.body.username;
-  var unnew = req.body.newusername;
-  var sql = "update `User` set UserName = '"+unnew+"' where UserName = '"+uncur+"';";
-  db.query(sql, function (err, result) {
-    if(err) throw err;
-    console.log("Username changed from " + uncur + " to " + unnew);
-  });
-  res.redirect('/account');
-});
-
-app.post('/account:password', (req, res) => { //spencer
-  var pcur = req.body.password;
-  var pnew = req.body.newpassword;
-  var sql = "update `User` set password = '"+pnew+"' where password = '"+pcur+"';";
-  db.query(sql, function (err, result) {
-    if(err) throw err;
-    console.log("Password changed from " + pcur + " to " + pnew);
-  });
-  res.redirect('/account');
-});
-
-
 
 app.listen("3000", () => {
   console.log("Server started on port 3000");
