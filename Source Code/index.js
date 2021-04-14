655d642ce43de2633c54199dcfc8dc24db55fb16
Ross Panning <grayaliens2@gmail.com>
2021-04-13 22:26:14 -0600

    Update index.js
    Added user methods

--- Source Code/index.js
+++ Source Code/index.js
@@ -3,6 +3,10 @@ const mysql = require("mysql");
 
 const app = express();
 
+
+var UserId = 0;
+var TheGameName = "junk";
+
 var bodyParser = require('body-parser'); //Ensure our body-parser tool has been added
 app.use(bodyParser.json());              // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -32,24 +36,27 @@ app.get("/", (req, res) => { //spencer
 app.post("/login", (req, res) => { //spencer
   const un = req.body.username;
   const pas = req.body.password;
-
-  const searchun = "select UserName from `User` where password = '"+pas+"';";
-
+  const searchun = "select UserID as user from User where (password ='"+pas+"') and (UserName ='"+un+"');";
   db.query(searchun, function (err, result) {
     if(err) {
       console.log(err);
-    } else {
-      if(result == un) {
-        res.render("/pages/home");
+    }
+    else {
+      if(!result.length) {
+        res.send("Incorrect Username or Password!");
       } else {
-        res.send("Incorrect Username or Password!")
+        result.forEach(function(item){
+          UserId = item.user;
+        });
+        res.redirect("/home");
       }
     }
   });
 });
 
 app.get("/collection", (req, res) => {
-  db.query('select * from games', function(err, rows, fields){
+  const qer = "select O.* from games O,  user P, usergame Q where (P.UserID = '"+UserId+"') and (P.UserID = Q.User_UserID) and (Q.Games_GameID = O.GameID);";
+  db.query(qer, function(err, rows, fields){
     if (err){
       console.log('error', err);
       res.render('pages/collection',{
@@ -66,21 +73,45 @@ app.get("/collection", (req, res) => {
   });
 });
 
+
+app.get("/medi", (req, res) =>{
+  const cof = "select GameID as theid from games where Title = '"+TheGameName+"';";
+  db.query(cof, function(err, result){
+    TheGameName = "junk";
+    if(err){
+      console.log('error', err);
+      res.redirect("/collection");
+    }
+    var temp = 0;
+    result.forEach(function(item){
+      temp = item.theid;
+    });
+    console.log(temp);
+    cif = "INSERT INTO `usergame` (User_UserID, Games_GameID) VALUES ('"+UserId+"', '"+temp+"')";
+    db.query(cif, function (err, result) {
+      if (err) throw err;
+      console.log("1 record inserted");
+    });
+    res.redirect("/collection");
+  });
+});
+
+
+
 app.post("/addgame", (req, res) => {
   var game_name = req.body.gamename;
+  TheGameName = req.body.gamename;
 	var game_year = req.body.yearpublished;
   var game_descrip = req.body.description;
-  console.log(game_name);
   var sql = "INSERT INTO `games` (Title, DateReleased, Description) VALUES ('"+game_name+"', '"+game_year+"', '"+ game_descrip+"')";
-  var sel = "SELECT * from games";
   db.query(sql, function (err, result) {
     if (err) throw err;
-    console.log("1 record inserted");
   });
-  res.redirect("/collection");
+  res.redirect("/medi");
 });
+
 app.get("/friends", (req, res) => {
-  db.query('select * from friendlist; select * from user', function(err, rows, fields){
+  db.query("select O.* from friendlist O,  user P, userfriends Q where (P.UserID = '"+UserId+"') and (P.UserID = Q.User_UserID) and (Q.FriendList_UserID = O.UserID); select * from user where UserID !='"+UserId+"'", function(err, rows, fields){
     if (err){
       console.log('error', err);
       res.render('pages/friends',{
@@ -112,23 +143,23 @@ app.post('/register', (req, res) => { //Spencer
   var dob = req.body.dob; //not null
 
   var nul = "null";
-  
+
   //var pn = req.body.phonenumber; //can be empty
   //var city = req.body.city; //can be empty
   //var state = req.body.state; //can be empty
   //var zip = req.body.zip; //can be empty
   //var country = req.body.country; //can be empty
-  
+
   //var acd = req.body.accountcreationdate; //rest nn
-  
+
   var sq1 = req.body.securityq1;
   var sa1 = req.body.securitya1;
-  
+
   //var sq2 = req.body.securityq2;
   //var sa2 = req.body.securitya2;
   //var sq3 = req.body.securityq3;
   //var sa3 = req.body.securitya3;
-  
+
   var sql = "insert into `User` (UserName, password, FirstName, LastName, Email, DOB, PhoneNumber, City, State, Zip, Country, AccountCreationDate, SecurityQuestion1, Answer1, SecurityQuestion2, Answer2, SecurityQuestion3, Answer3) VALUES ('"+un+"','"+pw+"','"+fn+"','"+ln+"','"+em+"','"+dob+"','"+nul+"','"+nul+"', '"+nul+"', '"+nul+"','"+nul+"','"+nul+"','"+sq1+"','"+sa1+"','"+nul+"','"+nul+"','"+nul+"','"+nul+"');";
   db.query(sql, function (err, result) {
     if (err) throw err;
@@ -140,4 +171,3 @@ app.post('/register', (req, res) => { //Spencer
 app.listen("3000", () => {
   console.log("Server started on port 3000");
 });
-
